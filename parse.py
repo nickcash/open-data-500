1d1d948c7b8af19aeb239935fa915bc29e065e3d
Thomas Levine <_@thomaslevine.com>
2014-02-01 08:22:58 +0000

    copy from r

--- parse.py
+++ parse.py
@@ -2,16 +2,20 @@
 import os
 from lxml.html import parse
 
+candidates_fields = [
+    ('name','a/h3/strong/text()'),
+    ('city','p[@class="m-homepage-list-location"]/text()'),
+    ('func','em/text()'),
+    ('desc','p[@class="m-homepage-list-desc"]/text()'),
+    ('preview.company','contains(@class, "preview-company")'),
+    ('survey.company','contains(@class, "survey-company")'),
+    ('href','a/@href'),
+]
+
 def candidates(html):
     for div in html.xpath('//div[@class="m-candidates isotopes-container"]/div'):
-        yield {'href':div.xpath('a/@href')[0]}
-        #   name = candidates.html.field('a/h3/strong/text()', xmlValue),
-        #   city = candidates.html.field('p[@class="m-homepage-list-location"]/text()', xmlValue),
-        #   func = candidates.html.field('em/text()', xmlValue),
-        #   desc = candidates.html.field('p[@class="m-homepage-list-desc"]/text()', xmlValue),
-        #   preview.company = candidates.html.field(preview.company.xpath),
-        #   survey.company = candidates.html.field(survey.company.xpath),
-        #   href = candidates.html.field('a/@href')
+        flatten = lambda x: x[0] if hasattr(x, '__len__') else x
+        yield {key: flatten(div.xpath(xpath)) for (key, xpath) in candidates_fields}
 
 def data():
     path_candidates = os.path.join('www.opendata500.com','candidates','index.html')
