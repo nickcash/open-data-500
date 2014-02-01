7595c826d544a0f1fddf93d44a7bd2751ac984cb
Thomas Levine <_@thomaslevine.com>
2014-02-01 07:47:11 +0000

    aoeu

--- parse.hs
+++ parse.hs
@@ -3,7 +3,14 @@
 import           AlphaHeavy.XPathParsers
 import qualified Data.ByteString as B
 
+xpath :: B.ByteString -> String -> IO [String]
+xpath html theXPath = do
+  values <- evaluateHTMLXPath theXPath "" html
+  return $ lines $ show values
+
 main = do
-  text <- B.readFile "www.opendata500.com/Vizzuality"
-  foo <- evaluateHTMLXPath "//strong/text()" "" text
+  html <- B.readFile "www.opendata500.com/Vizzuality"
+  foo <- evaluateHTMLXPath "//strong/text()" "" html
   putStrLn $ show foo
+  f <- xpath html "//strong/text()"
+  putStrLn $ f !! 1
